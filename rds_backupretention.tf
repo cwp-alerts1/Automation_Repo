9243aff9ec86b2e29f765e644d6b66c644853c9a
cwp-alerts1 <103568629+cwp-alerts1@users.noreply.github.com>
2023-07-19 12:49:53 +0530

    Commiting file for scan

---
+++ rds_backupretention.tf
@@ -0,0 +1,8 @@
+
+resource "aws_rds_cluster" "education" {
+    name                      = "education"
+    instance_class            = "db.t3.micro"
+    allocated_storage         = 10
+    storage_encrypted         = false
+    #kms_key_id = aws_kms_key.mykey.arn
+}
